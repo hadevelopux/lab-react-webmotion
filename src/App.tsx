74ae9af33d431eeb46002bf3d2bee1a9d7fd0a76
Harry J. Arena <harryalejandro@gmail.com>
2025-04-23 16:21:03 -0400

    organized the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Contact from './pages/Contact/Contact'
-import PageTransition from './components/PageTransition'
+import PageTransition from './components/PageTransition/PageTransition'
 import styles from './App.module.scss'
 
 const App: React.FC = () => {
